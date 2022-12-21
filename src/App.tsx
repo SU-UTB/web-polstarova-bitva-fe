c4f6f83b7dce6c76cd2c95b65c73256565f49bf4
David Sedlář <sedlardavid.cz@gmail.com>
2022-12-21 16:17:44 +0100

    base info added

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,16 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      
+      <p className="title">Největší polštářová bitva v ČR</p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <h1>
+         1. 4. 2023
+        </h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>
+        Více informací již brzy...
+      </h3>
     </div>
   )
 }
