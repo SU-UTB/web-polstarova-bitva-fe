af9a4d0f3ec691d3ffa7ea3bac76d82cbaecc620
Juřička, Tomáš <tju@ciklum.com>
2023-11-12 16:02:40 +0100

    Prettier etc...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import robotsTxt from "astro-robots-txt";
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import robotsTxt from 'astro-robots-txt';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://polstarovabitva.cz/',
-  integrations: [tailwind(), react(), robotsTxt(), sitemap()]
+  integrations: [tailwind(), react(), robotsTxt(), sitemap()],
 });
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,173 +1,169 @@
-import { Menu, Transition } from '@headlessui/react'
-import React from 'react'
-import { SuLogo } from '../atoms/SuLogo'
+import { Menu, Transition } from '@headlessui/react';
+import React from 'react';
+import { SuLogo } from '../atoms/SuLogo';
 
 export const Header = () => {
-	return (
-		<div className="relative z-50">
-			<Menu
-				as="header"
-				className="mx-auto flex w-full items-center justify-between px-5 py-4 lg:container md:px-10 md:py-8"
-			>
-				{({ open }) => {
-					if (typeof window !== 'undefined') document?.body.classList.toggle('overflow-y-hidden', open)
+  return (
+    <div className="relative z-50">
+      <Menu
+        as="header"
+        className="mx-auto flex w-full items-center justify-between px-5 py-4 lg:container md:px-10 md:py-8"
+      >
+        {({ open }) => {
+          if (typeof window !== 'undefined') document?.body.classList.toggle('overflow-y-hidden', open);
 
-					return (
-						<>
-							<a
-								href="/"
-								className="relative z-50 flex items-center gap-4 font-bold tracking-wide sm:text-lg sm:text-xl"
-							>
-								<SuLogo className="h-8" />
-								<span>Největší polštářová bitva v ČR</span>
-							</a>
-							{/* Mobile menu button */}
-							<Menu.Button
-								className="relative z-50  h-fit w-fit rounded-lg  p-1 text-gray-800 lg:hidden"
-								title="Navigace"
-							>
-								{({ open }) => (
-									<>
-										<div className="relative h-6 w-6">
-											<Transition
-												className="absolute inset-0"
-												enter="transition duration-500"
-												enterFrom="opacity-0"
-												enterTo="opacity-100"
-												leave="transition duration-500"
-												leaveFrom="opacity-100"
-												leaveTo="opacity-0"
-												show={open}
-											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 24 24"
-													strokeWidth={2.5}
-													stroke="currentColor"
-													className="h-6 w-6"
-												>
-													<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-												</svg>
-											</Transition>
-											<Transition
-												className="absolute inset-0"
-												enter="transition duration-500"
-												enterFrom="opacity-0"
-												enterTo="opacity-100"
-												leave="transition duration-500"
-												leaveFrom="opacity-100"
-												leaveTo="opacity-0"
-												show={!open}
-											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													fill="none"
-													viewBox="0 0 24 24"
-													strokeWidth={2}
-													stroke="currentColor"
-													className="h-6 w-6"
-												>
-													<path
-														strokeLinecap="round"
-														strokeLinejoin="round"
-														d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-													/>
-												</svg>
-											</Transition>
-										</div>
-									</>
-								)}
-							</Menu.Button>
-							{/* Mobile menu */}
-							<Transition
-								enter="transition-all duration-700 linear"
-								enterFrom="transform h-0"
-								enterTo="transform h-screen"
-								leave="transition-all duration-[800ms] ease-out "
-								leaveFrom="transform h-screen"
-								leaveTo="transform h-0"
-								as={React.Fragment}
-							>
-								<Menu.Items as="div" className="fixed inset-0 w-screen bg-gray-100">
-									<nav className="flex h-full w-full flex-col items-center justify-center space-y-6 text-center text-xl font-medium tracking-wider text-gray-800">
-										<Transition.Child
-											enter={`transition duration-700 linear delay-[200ms]`}
-											enterFrom="opacity-0"
-											enterTo="opacity-100"
-											leave="transition duration-300 linear"
-											leaveFrom="opacity-100"
-											leaveTo="opacity-0"
-											as={React.Fragment}
-										>
-											<Menu.Item>
-												<a href="/kontakt" className="p-2">
-													Kontakt
-												</a>
-											</Menu.Item>
-										</Transition.Child>
+          return (
+            <>
+              <a
+                href="/"
+                className="relative z-50 flex items-center gap-4 font-bold tracking-wide sm:text-lg sm:text-xl"
+              >
+                <SuLogo className="h-8" />
+                <span>Největší polštářová bitva v ČR</span>
+              </a>
+              {/* Mobile menu button */}
+              <Menu.Button
+                className="relative z-50  h-fit w-fit rounded-lg  p-1 text-gray-800 lg:hidden"
+                title="Navigace"
+              >
+                {({ open }) => (
+                  <>
+                    <div className="relative h-6 w-6">
+                      <Transition
+                        className="absolute inset-0"
+                        enter="transition duration-500"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="transition duration-500"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                        show={open}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={2.5}
+                          stroke="currentColor"
+                          className="h-6 w-6"
+                        >
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                      </Transition>
+                      <Transition
+                        className="absolute inset-0"
+                        enter="transition duration-500"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="transition duration-500"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                        show={!open}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={2}
+                          stroke="currentColor"
+                          className="h-6 w-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                          />
+                        </svg>
+                      </Transition>
+                    </div>
+                  </>
+                )}
+              </Menu.Button>
+              {/* Mobile menu */}
+              <Transition
+                enter="transition-all duration-700 linear"
+                enterFrom="transform h-0"
+                enterTo="transform h-screen"
+                leave="transition-all duration-[800ms] ease-out "
+                leaveFrom="transform h-screen"
+                leaveTo="transform h-0"
+                as={React.Fragment}
+              >
+                <Menu.Items as="div" className="fixed inset-0 w-screen bg-gray-100">
+                  <nav className="flex h-full w-full flex-col items-center justify-center space-y-6 text-center text-xl font-medium tracking-wider text-gray-800">
+                    <Transition.Child
+                      enter={`transition duration-700 linear delay-[200ms]`}
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="transition duration-300 linear"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      as={React.Fragment}
+                    >
+                      <Menu.Item>
+                        <a href="/kontakt" className="p-2">
+                          Kontakt
+                        </a>
+                      </Menu.Item>
+                    </Transition.Child>
 
-										<Transition.Child
-											enter={`transition duration-700 linear delay-[400ms]`}
-											enterFrom="opacity-0"
-											enterTo="opacity-100"
-											leave="transition duration-300 linear"
-											leaveFrom="opacity-100"
-											leaveTo="opacity-0"
-											as={React.Fragment}
-										>
-											<Menu.Item>
-												<a
-													href="/partneri"
-													className="rounded-lg border border-gray-800 px-8 py-3 text-gray-800"
-												>
-													Pro partnery
-												</a>
-											</Menu.Item>
-										</Transition.Child>
+                    <Transition.Child
+                      enter={`transition duration-700 linear delay-[400ms]`}
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="transition duration-300 linear"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      as={React.Fragment}
+                    >
+                      <Menu.Item>
+                        <a href="/partneri" className="rounded-lg border border-gray-800 px-8 py-3 text-gray-800">
+                          Pro partnery
+                        </a>
+                      </Menu.Item>
+                    </Transition.Child>
 
-										<Transition.Child
-											enter={`transition duration-700 linear delay-[600ms]`}
-											enterFrom="opacity-0"
-											enterTo="opacity-100"
-											leave="transition duration-300 linear"
-											leaveFrom="opacity-100"
-											leaveTo="opacity-0"
-											as={React.Fragment}
-										>
-											<Menu.Item>
-												<a href="/ucastnici" className="rounded-lg bg-gray-800 px-8 py-3 text-gray-200">
-													Pro účastníky
-												</a>
-											</Menu.Item>
-										</Transition.Child>
-									</nav>
-								</Menu.Items>
-							</Transition>
-							{/* Desktop menu */}
-							<nav className="hidden items-center space-x-4 text-lg font-medium tracking-wide text-gray-700 lg:flex">
-								<a href="/kontakt" className="">
-									Kontakt
-								</a>
+                    <Transition.Child
+                      enter={`transition duration-700 linear delay-[600ms]`}
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="transition duration-300 linear"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                      as={React.Fragment}
+                    >
+                      <Menu.Item>
+                        <a href="/ucastnici" className="rounded-lg bg-gray-800 px-8 py-3 text-gray-200">
+                          Pro účastníky
+                        </a>
+                      </Menu.Item>
+                    </Transition.Child>
+                  </nav>
+                </Menu.Items>
+              </Transition>
+              {/* Desktop menu */}
+              <nav className="hidden items-center space-x-4 text-lg font-medium tracking-wide text-gray-700 lg:flex">
+                <a href="/kontakt" className="px-2">
+                  Kontakt
+                </a>
+                <a href="/blog" className="px-2">
+                  Blog
+                </a>
+                <a href="/partneri" className="-my-2.5 rounded-lg border border-gray-800  px-6 py-2.5 text-gray-800">
+                  Pro Partnery
+                </a>
 
-								<a
-									href="/partneri"
-									className="-my-2.5 rounded-lg border border-gray-800  px-6 py-2.5 text-gray-800"
-								>
-									Pro Partnery
-								</a>
-
-								<a
-									href="/ucastnici"
-									className="-my-2.5 rounded-lg border border-transparent bg-gray-800 px-6 py-2.5 text-gray-200"
-								>
-									Pro účastníky
-								</a>
-							</nav>
-						</>
-					)
-				}}
-			</Menu>
-		</div>
-	)
-}
+                <a
+                  href="/ucastnici"
+                  className="-my-2.5 rounded-lg border border-transparent bg-gray-800 px-6 py-2.5 text-gray-200"
+                >
+                  Pro účastníky
+                </a>
+              </nav>
+            </>
+          );
+        }}
+      </Menu>
+    </div>
+  );
+};
